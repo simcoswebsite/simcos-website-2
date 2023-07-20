c6d1580eab79323d4d96a01bbae2d3025b831325
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-07-20 19:05:18 -0400

    debugging

--- app/(routes)/cart/components/Summary.tsx
+++ app/(routes)/cart/components/Summary.tsx
@@ -33,6 +33,7 @@ const Summary = () => {
     const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
       productIds: items.map((item) => item.id)
     });
+    console.log(response)
 
     window.location = response.data.url;
   }
